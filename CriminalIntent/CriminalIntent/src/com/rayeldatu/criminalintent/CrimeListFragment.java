1fca1bbf6dfc6682ba755e4c8d491eadb41c1a24
rdatu <r.datu@klab.com>
2015-07-03 11:58:47 +0800

    Action Bar Menus Added (showsubtitle/hidesubtitle and new crime)

--- CriminalIntent/CriminalIntent/src/com/rayeldatu/criminalintent/CrimeListFragment.java
+++ CriminalIntent/CriminalIntent/src/com/rayeldatu/criminalintent/CrimeListFragment.java
@@ -2,9 +2,15 @@ package com.rayeldatu.criminalintent;
 
 import java.util.ArrayList;
 
+import android.annotation.TargetApi;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -14,6 +20,7 @@ import android.widget.TextView;
 
 public class CrimeListFragment extends ListFragment {
 	private ArrayList<Crime> mCrimes;
+	private boolean mSubtitleVisible;
 	private static final String TAG = "CrimeListFragment";
 	private static final int REQUEST_CRIME = 1;
 
@@ -56,6 +63,10 @@ public class CrimeListFragment extends ListFragment {
 
 		CrimeAdapter adapter = new CrimeAdapter(mCrimes);
 		setListAdapter(adapter);
+
+		setHasOptionsMenu(true);
+		setRetainInstance(true);
+		mSubtitleVisible = false;
 	}
 
 	@Override
@@ -68,8 +79,56 @@ public class CrimeListFragment extends ListFragment {
 
 	@Override
 	public void onActivityResult(int requestCode, int resultCode, Intent data) {
-		if (requestCode == REQUEST_CRIME) {
+		((CrimeAdapter) getListAdapter()).notifyDataSetChanged();
+	}
 
+	@Override
+	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+		super.onCreateOptionsMenu(menu, inflater);
+		inflater.inflate(R.menu.fragment_crime_list, menu);
+		MenuItem showSubtitle = menu.findItem(R.id.menu_item_show_subtitle);
+		if(mSubtitleVisible  && showSubtitle != null){
+			showSubtitle.setTitle(R.string.hide_subtitle);
+		}
+	}
+
+	@TargetApi(11)
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup parent,Bundle savedInstanceState){
+		View v = super.onCreateView(inflater, parent, savedInstanceState);
+		
+		if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB){
+			if(mSubtitleVisible){
+				getActivity().getActionBar().setSubtitle(R.string.subtitle);
+			}
+		}
+		return v;
+	}
+	
+	@TargetApi(11)
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.menu_item_new_crime:
+			Crime crime = new Crime();
+			CrimeLab.get(getActivity()).addCrime(crime);
+			Intent i = new Intent(getActivity(), CrimePagerActivity.class);
+			i.putExtra(CrimeFragment.EXTRA_CRIME_ID, crime.getId());
+			startActivityForResult(i, 0);
+			return true;
+		case R.id.menu_item_show_subtitle:
+			if (getActivity().getActionBar().getSubtitle() == null) {
+				getActivity().getActionBar().setSubtitle(R.string.subtitle);
+				mSubtitleVisible = true;
+				item.setTitle(R.string.hide_subtitle);
+			} else {
+				getActivity().getActionBar().setSubtitle(null);
+				mSubtitleVisible = false;
+				item.setTitle(R.string.show_subtitle);
+			}
+
+		default:
+			return super.onOptionsItemSelected(item);
 		}
 	}
 
