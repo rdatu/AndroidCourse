7d8f6103398ab2ba993264d6878aaacd5f186805
rdatu <r.datu@klab.com>
2015-07-06 17:25:29 +0800

    Nerd Launcher can find activities

--- NerdLauncher/NerdLauncher/src/com/rayeldatu/nerdlauncher/NerdLauncherActivity.java
+++ NerdLauncher/NerdLauncher/src/com/rayeldatu/nerdlauncher/NerdLauncherActivity.java
@@ -1,35 +1,13 @@
 package com.rayeldatu.nerdlauncher;
 
-import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v4.app.Fragment;
 
+public class NerdLauncherActivity extends SingleFragmentActivity {
 
-public class NerdLauncherActivity extends ActionBarActivity {
+	@Override
+	protected Fragment createFragment() {
+		// TODO Auto-generated method stub
+		return new NerdLauncherFragment();
+	}
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_nerd_launcher);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.nerd_launcher, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
