236affc6ef27160f7f44bab3ca57b66fba91b073
rdatu <r.datu@klab.com>
2015-07-06 15:32:59 +0800

    Getting Data from the Contact List

--- CriminalIntent/CriminalIntent/src/com/rayeldatu/criminalintent/CrimeFragment.java
+++ CriminalIntent/CriminalIntent/src/com/rayeldatu/criminalintent/CrimeFragment.java
@@ -7,7 +7,9 @@ import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
@@ -181,8 +183,8 @@ public class CrimeFragment extends Fragment {
 
 			}
 		});
-		
-		if(mCrime.getSuspect() != null)
+
+		if (mCrime.getSuspect() != null)
 			mSuspectButton.setText(mCrime.getSuspect());
 
 		return v;
@@ -249,6 +251,22 @@ public class CrimeFragment extends Fragment {
 				mCrime.setPhoto(p);
 				showPhoto();
 			}
+		} else if (requestCode == REQUEST_CONTACT) {
+			Uri contactUri = data.getData();
+
+			String[] queryFields = new String[] { ContactsContract.Contacts.DISPLAY_NAME };
+
+			Cursor c = getActivity().getContentResolver().query(contactUri,
+					queryFields, null, null, null);
+			if(c.getCount() == 0 ){
+				c.close();
+				return;
+			}
+			c.moveToFirst();
+			String suspect = c.getString(0);
+			mCrime.setSuspect(suspect);
+			mSuspectButton.setText(suspect);
+			c.close();
 		}
 	}
 
