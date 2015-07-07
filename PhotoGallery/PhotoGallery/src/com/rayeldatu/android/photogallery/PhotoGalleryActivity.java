77faaf38a708af2745c9e108bf62825f269e0e5d
rdatu <r.datu@klab.com>
2015-07-07 17:17:03 +0800

    Retrieving data  from Network (flickr)  using its API

--- PhotoGallery/PhotoGallery/src/com/rayeldatu/android/photogallery/PhotoGalleryActivity.java
+++ PhotoGallery/PhotoGallery/src/com/rayeldatu/android/photogallery/PhotoGalleryActivity.java
@@ -1,35 +1,13 @@
 package com.rayeldatu.android.photogallery;
 
-import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v4.app.Fragment;
 
+public class PhotoGalleryActivity extends SingleFragmentActivity {
 
-public class PhotoGalleryActivity extends ActionBarActivity {
+	@Override
+	protected Fragment createFragment() {
+		// TODO Auto-generated method stub
+		return new PhotoGalleryFragment();
+	}
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_photo_gallery);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.photo_gallery, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
