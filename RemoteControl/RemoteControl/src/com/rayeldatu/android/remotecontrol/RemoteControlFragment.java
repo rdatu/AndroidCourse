6804cb3b4ca24533ac3cda771010176e0de36772
rdatu <r.datu@klab.com>
2015-07-07 11:21:50 +0800

    0 , 1 , Enter Button Listeners

--- RemoteControl/RemoteControl/src/com/rayeldatu/android/remotecontrol/RemoteControlFragment.java
+++ RemoteControl/RemoteControl/src/com/rayeldatu/android/remotecontrol/RemoteControlFragment.java
@@ -1,7 +1,66 @@
 package com.rayeldatu.android.remotecontrol;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class RemoteControlFragment extends Fragment {
 
+	private TextView mSelectedTextView;
+	private TextView mWorkingTextView;
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup parent,
+			Bundle savedInstanceState) {
+		View v = inflater.inflate(R.layout.fragment_remote_control, parent,
+				false);
+		mSelectedTextView = (TextView) v
+				.findViewById(R.id.fragment_remote_control_selectedTextView);
+		mWorkingTextView = (TextView) v
+				.findViewById(R.id.fragment_remote_control_workingTextView);
+
+		View.OnClickListener numberButtonListener = new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				TextView textView = (TextView) v;
+				String working = mWorkingTextView.getText().toString();
+				String text = textView.getText().toString();
+				if (working.equals("0"))
+					mWorkingTextView.setText(text);
+				else
+					mWorkingTextView.setText(working + text);
+			}
+		};
+
+		Button zeroButton = (Button) v
+				.findViewById(R.id.fragment_remote_control_zeroButton);
+		zeroButton.setOnClickListener(numberButtonListener);
+		Button oneButton = (Button) v
+				.findViewById(R.id.fragment_remote_control_oneButton);
+		oneButton.setOnClickListener(numberButtonListener);
+
+		Button enterButton = (Button) v
+				.findViewById(R.id.fragment_remote_control_enterButton);
+		enterButton.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				CharSequence working = mWorkingTextView.getText();
+				if (working.length() > 0) {
+					mSelectedTextView.setText(working);
+				}
+				mWorkingTextView.setText("0");
+
+			}
+		});
+
+		return v;
+	}
 }
