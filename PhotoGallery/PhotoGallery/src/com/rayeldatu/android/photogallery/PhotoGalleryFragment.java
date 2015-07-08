8ab028204c8991ae37fcd9dc398eb5460120bb8a
rdatu <r.datu@klab.com>
2015-07-08 14:57:09 +0800

    Search Engine in Flickr App

--- PhotoGallery/PhotoGallery/src/com/rayeldatu/android/photogallery/PhotoGalleryFragment.java
+++ PhotoGallery/PhotoGallery/src/com/rayeldatu/android/photogallery/PhotoGalleryFragment.java
@@ -2,124 +2,166 @@ package com.rayeldatu.android.photogallery;
 
 import java.util.ArrayList;
 
-import android.graphics.Bitmap;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.app.SearchManager;
+import android.app.SearchableInfo;
+import android.content.ComponentName;
+import android.content.Context;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
-import android.util.Log;
+
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.SearchView;
 
 public class PhotoGalleryFragment extends Fragment {
-	GridView mGridView;
-	ArrayList<GalleryItem> mItems;
+    GridView mGridView;
+    ArrayList<GalleryItem> mItems;
+    ThumbnailDownloader mThumbnailThread;
 
-	ThumbnailDownloader<ImageView> mThumbnailThread;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        setRetainInstance(true);
+        setHasOptionsMenu(true);
 
-	private static final String TAG = "PhotoGalleryFragment";
+        updateItems();
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setRetainInstance(true);
-		new FetchItemsTask().execute();
+        mThumbnailThread = new ThumbnailDownloader(new Handler());
+        mThumbnailThread.start();
+    }
+    
+    public void updateItems() {
+        new FetchItemsTask().execute();
+    }
 
-		mThumbnailThread = new ThumbnailDownloader<ImageView>(new Handler());
-		mThumbnailThread
-				.setListener(new ThumbnailDownloader.Listener<ImageView>() {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_photo_gallery, container, false);
+        
+        mGridView = (GridView)v.findViewById(R.id.gridView);
+        
+        setupAdapter();
+        
+        return v;
+    }
+    
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mThumbnailThread.quit();
+    }
+    
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        mThumbnailThread.clearQueue();
+    }
+    
+    @Override
+    @TargetApi(11)
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.fragment_photo_gallery, menu);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            // pull out the SearchView
+            MenuItem searchItem = menu.findItem(R.id.menu_item_search);
+            SearchView searchView = (SearchView)searchItem.getActionView();
 
-					@Override
-					public void onThumbnailDownloaded(ImageView imageView,
-							Bitmap thumbnail) {
-						if (isVisible()) {
-							imageView.setImageBitmap(thumbnail);
-						}
-						// TODO Auto-generated method stub
+            // get the data from our searchable.xml as a SearchableInfo
+            SearchManager searchManager = (SearchManager)getActivity()
+                .getSystemService(Context.SEARCH_SERVICE);
+            ComponentName name = getActivity().getComponentName();
+            SearchableInfo searchInfo = searchManager.getSearchableInfo(name);
 
-					}
-				});
-		mThumbnailThread.start();
-		mThumbnailThread.getLooper();
-		Log.i(TAG, "Background thread started");
-	}
+            searchView.setSearchableInfo(searchInfo);
+        }
+    }
 
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup parent,
-			Bundle savedInstanceState) {
-		View v = inflater.inflate(R.layout.fragment_photo_gallery, parent,
-				false);
-		mGridView = (GridView) v.findViewById(R.id.gridView);
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_item_search:
+                getActivity().onSearchRequested();
+                return true;
+            case R.id.menu_item_clear:
+                PreferenceManager.getDefaultSharedPreferences(getActivity())
+                    .edit()
+                    .putString(FlickrFetchr.PREF_SEARCH_QUERY, null)
+                    .commit();
+                updateItems();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
-		setupAdapter();
-		return v;
-	}
+    void setupAdapter() {
+        if (getActivity() == null || mGridView == null) return;
+        
+        if (mItems != null) {
+            mGridView.setAdapter(new GalleryItemAdapter(mItems));
+        } else {
+            mGridView.setAdapter(null);
+        }
+    }
 
-	@Override
-	public void onDestroy() {
-		super.onDestroy();
-		mThumbnailThread.quit();
-		Log.i(TAG, "Background Thread is destroyed");
-	}
+    private class FetchItemsTask extends AsyncTask<Void,Void,ArrayList<GalleryItem>> {
+        @Override
+        protected ArrayList<GalleryItem> doInBackground(Void... params) {            
+            Activity activity = getActivity();
+            if (activity == null) 
+                return new ArrayList<GalleryItem>();
 
-	void setupAdapter() {
-		if (getActivity() == null || mGridView == null)
-			return;
-		if (mItems != null) {
-			mGridView.setAdapter(new GalleryItemAdapter(mItems));
+            String query = PreferenceManager.getDefaultSharedPreferences(activity)
+                .getString(FlickrFetchr.PREF_SEARCH_QUERY, null);
+            if (query != null) {
+                return new FlickrFetchr().search(query);
+            } else {
+                return new FlickrFetchr().fetchItems();
+            }
+        }
 
-		} else {
-			mGridView.setAdapter(null);
-		}
-	}
+        @Override
+        protected void onPostExecute(ArrayList<GalleryItem> items) {
+            mItems = items;
+            setupAdapter();
+        }
+    }
+    
+    private class GalleryItemAdapter extends ArrayAdapter<GalleryItem> {
+        public GalleryItemAdapter(ArrayList<GalleryItem> items) {
+            super(getActivity(), 0, items);
+        }
 
-	private class FetchItemsTask extends
-			AsyncTask<Void, Void, ArrayList<GalleryItem>> {
-
-		@Override
-		protected ArrayList<GalleryItem> doInBackground(Void... params) {
-			// TODO Auto-generated method stub
-			return new FlickrFetchr().fetchItems();
-
-		}
-
-		@Override
-		protected void onPostExecute(ArrayList<GalleryItem> items) {
-			mItems = items;
-			setupAdapter();
-		}
-
-	}
-
-	private class GalleryItemAdapter extends ArrayAdapter<GalleryItem> {
-		public GalleryItemAdapter(ArrayList<GalleryItem> items) {
-			super(getActivity(), 0, items);
-		}
-
-		@Override
-		public View getView(int position, View convertView, ViewGroup parent) {
-			if (convertView == null) {
-				convertView = getActivity().getLayoutInflater().inflate(
-						R.layout.gallery_item, parent, false);
-			}
-
-			ImageView imageView = (ImageView) convertView
-					.findViewById(R.id.gallery_item_imageView);
-			imageView.setImageResource(R.drawable.brian_up_close);
-			GalleryItem item = getItem(position);
-			mThumbnailThread.queueThumbnail(imageView, item.getUrl());
-
-			return convertView;
-		}
-	}
-	@Override
-	public void onDestroyView(){
-		super.onDestroyView();
-		mThumbnailThread.clearQueue();
-		
-	}
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                convertView = getActivity().getLayoutInflater()
+                        .inflate(R.layout.gallery_item, parent, false);
+            }
+            
+            GalleryItem item = getItem(position);
+            ImageView imageView = (ImageView)convertView
+                    .findViewById(R.id.gallery_item_imageView);
+            imageView.setImageResource(R.drawable.brian_up_close);
+            mThumbnailThread.queueThumbnail(imageView, item.getUrl());
+            
+            return convertView;
+        }
+    }
 }
--- PhotoGallery/PhotoGallery/src/com/rayeldatu/android/photogallery/SingleFragmentActivity.java
+++ PhotoGallery/PhotoGallery/src/com/rayeldatu/android/photogallery/SingleFragmentActivity.java
@@ -1,26 +1,26 @@
 package com.rayeldatu.android.photogallery;
 
 import android.os.Bundle;
+
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 
 public abstract class SingleFragmentActivity extends FragmentActivity {
-	protected abstract Fragment createFragment();
-	
-	protected int getLayoutResId(){
-		return R.layout.activity_fragment;
-	}
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(getLayoutResId());	
-		android.support.v4.app.FragmentManager fm = getSupportFragmentManager();
-		Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+    protected abstract Fragment createFragment();
 
-		if (fragment == null) {
-			fragment = createFragment();
-			fm.beginTransaction().add(R.id.fragmentContainer, fragment)
-					.commit();
-		}
-	}
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+        FragmentManager manager = getSupportFragmentManager();
+        Fragment fragment = manager.findFragmentById(R.id.fragmentContainer);
+
+        if (fragment == null) {
+            fragment = createFragment();
+            manager.beginTransaction()
+                .add(R.id.fragmentContainer, fragment)
+                .commit();
+        }
+    }
 }
