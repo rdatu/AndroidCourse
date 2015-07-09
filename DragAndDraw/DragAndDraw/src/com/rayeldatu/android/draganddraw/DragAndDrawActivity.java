eceece3b4fef9604c065215e32425b6e3f32c5cb
rdatu <r.datu@klab.com>
2015-07-09 11:03:05 +0800

    Created new Project: DragAndDraw

---
+++ DragAndDraw/DragAndDraw/src/com/rayeldatu/android/draganddraw/DragAndDrawActivity.java
@@ -0,0 +1,12 @@
+package com.rayeldatu.android.draganddraw;
+
+import android.support.v4.app.Fragment;
+
+public class DragAndDrawActivity extends SingleFragmentActivity {
+
+	@Override
+	protected Fragment createFragment() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
